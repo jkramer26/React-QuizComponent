60e01b7f5fb484f53f46ea925f278a3ca7527edc
Jessica Kramer <kra9744@s287410.nml.com>
2020-01-23 14:47:53 -0600

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
     }
 
     showNextQuestion() {
-        this.setState((state) => {
+        this.setState(( state ) => {
             return { quiz_position: state.quiz_position + 1 };
         });
     }
