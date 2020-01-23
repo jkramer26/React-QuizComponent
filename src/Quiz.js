e148cdd64ffaa9996415b6d80042c0030e1e4a7d
Jessica Kramer <kra9744@s287410.nml.com>
2020-01-23 12:33:18 -0600

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react' 
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd'
+
 let quizData = require('./quiz_data.json')
  
 
@@ -10,10 +12,11 @@ class Quiz extends Component {
     }
 
     render() {
-        
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
         return (
-            <div> 
-               <QuizQuestion className="QuizQuestion" quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} {...quizData} />
+            <div>
+                {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion className="QuizQuestion" quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} {...quizData} />}
             </div>  
         )
     }
